07750dbb64986dd81b439b53b9baf131fccbb826
unknown <ekftjd170@gmail.com>
2023-07-10 15:28:20 +0900

    add blueprint2

--- views/main_views.py
+++ views/main_views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-#Blueprint 클래스를 통해 임의의 객체 생성
+# Blueprint 클래스를 통해 임의의 객체 생성
+# bp =  Blueprint('앱 내부에서 부를 별명', 전달되는 파일명, url_prefix="해당 블루프린트가 전달할 최상위경로")
 bp =  Blueprint('main', __name__, url_prefix="/")
 
 # 라우팅 주소를 만들때는 '/경로명'까지만 적어주기
